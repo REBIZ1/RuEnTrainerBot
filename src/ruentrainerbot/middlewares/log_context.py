57223c82839f6a76593a4ad946b20d225189ceb2
Arthur <arthur155188@gmail.com>
2026-01-09 22:25:57 +0500

    Добавлен middleware для логирования контекста Telegram апдейтов

---
+++ src/ruentrainerbot/middlewares/log_context.py
@@ -0,0 +1,33 @@
+import structlog
+from typing import Any, Awaitable, Callable, Dict, Optional
+from aiogram import BaseMiddleware
+from aiogram.types import TelegramObject, Update
+
+
+class LogContextMiddleware(BaseMiddleware):
+    """
+    Автоматически добавляет
+    контекст текущего Telegram апдейта в логи structlog
+    """
+    async def __call__(
+            self,
+            handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
+            event: TelegramObject,
+            data: Dict[str, Any],
+    ) -> Any:
+        ctx: Dict[str, Any] = {}
+        update: Optional[Update] = data.get('event_update')
+        from_user = getattr(event, 'from_user', None)
+        chat = getattr(event, 'chat', None)
+        if update is not None:
+            ctx['update_id'] = update.update_id
+        if from_user is not None:
+            ctx['user_id'] = from_user.id
+        if chat is not None:
+            ctx['chat_id'] = chat.id
+
+        structlog.contextvars.bind_contextvars(**ctx)
+        try:
+            return await handler(event, data)
+        finally:
+            structlog.contextvars.clear_contextvars()
